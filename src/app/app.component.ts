62188bf00e22b6c42c6f7309d7f2e3742931a89e
sebC18 <capdeville.sebastien@gmail.com>
2020-09-24 11:34:28 +0200

    10 - Observable et destruction pour eviter les problèmes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
+import { Observable, Subscription } from 'rxjs';
+import 'rxjs/Rx';
 import { AppareilService } from './services/appareil.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  
+  secondes : number;
+  counterSubscription : Subscription;
+
   constructor(){
-    
+
+  }
+
+  ngOnInit(){
+    const counter = Observable.interval(1000);
+    this.counterSubscription = counter.subscribe(
+      (value : number ) => {
+        this.secondes = value;
+      }
+    );
   }
   
+  ngOnDestroy(){
+    this.counterSubscription.unsubscribe();
+  }
 }
